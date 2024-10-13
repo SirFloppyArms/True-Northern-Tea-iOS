cd73db4375f490043ec81569482b4a1ff311d091
SirFloppyArms <106633884+SirFloppyArms@users.noreply.github.com>
2024-10-13 14:22:51 -0500

    Attempt #1 at cart feature

--- True Northern Tea/ContentView.swift
+++ True Northern Tea/ContentView.swift
@@ -5,16 +5,28 @@
 //  Created by Nolan Law on 2024-10-12.
 //
 
+// MARK: - STARTING
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var showMenu = false
+    @State private var cartItems: [CartItem] = []
+    @State private var showCart = false
+    @State private var showAddConfirmation = false
+
+    struct CartItem: Identifiable {
+        var id = UUID()
+        var title: String
+        var message: String
+        var image: String
+        var description: String
+    }
 
     var body: some View {
         ScrollView(.vertical) {
-            // HEADER
             VStack {
-                Color.clear.frame(height: 50)
+                // MARK: - HEADER
                 HStack {
                     Spacer()
                     HStack(spacing: 20) {
@@ -24,6 +36,11 @@ struct ContentView: View {
                         Image("cart-icon")
                             .resizable()
                             .frame(width: 25, height: 25)
+                            .onTapGesture {
+                                withAnimation {
+                                    showCart.toggle()
+                                }
+                            }
                         Image("menu-icon")
                             .resizable()
                             .frame(width: 25, height: 25)
@@ -38,6 +55,7 @@ struct ContentView: View {
                 .padding(.vertical)
                 .overlay(Divider(), alignment: .bottom)
 
+                //MARK: - MENU & CART
                 if showMenu {
                     VStack(alignment: .leading) {
                         menuLink("Home", sectionId: "section1")
@@ -49,9 +67,20 @@ struct ContentView: View {
                     }
                     .background(Color.white.shadow(radius: 10))
                     .transition(.move(edge: .top))
+                    .zIndex(1) // Ensure it displays over the banner
                 }
 
-                // BANNER
+                if showCart {
+                    VStack {
+                        cartPopup
+                            .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure it takes up full screen
+                            .background(Color.black.opacity(0.7)) // Dim background
+                            .edgesIgnoringSafeArea(.all) // Cover entire screen
+                    }
+                    .transition(.move(edge: .bottom))
+                }
+
+                // MARK: - BANNER
                 VStack(spacing: 15) {
                     Text("True Northern Tea")
                         .font(.custom("Baskerville-Bold", size: 60))
@@ -66,7 +95,7 @@ struct ContentView: View {
                         .padding(.horizontal)
 
                     Button(action: {
-                        print("Order Now button clicked")
+                        showCart.toggle()
                     }) {
                         Text("Order Now")
                             .font(.custom("HelveticaNeue", size: 18))
@@ -82,16 +111,16 @@ struct ContentView: View {
                 .padding(.top, 20)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .id("section1")
-            }
-            .background {
-                Image("banner")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .overlay(Color.black.opacity(0.3))
+                .background {
+                    Image("banner")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .overlay(Color.black.opacity(0.3))
+                }
             }
 
-            // ALLERGEN ALERT SECTION
+            // MARK: - ALLERGEN
             SectionView(title: "Allergen Alert", backgroundColor: .white, textColor: .black) {
                 Text("Despite our best efforts, We cannot guarantee that the tea has not come into contact with peanuts, nuts & other allergens.")
                     .font(.custom("Georgia", size: 16))
@@ -100,20 +129,20 @@ struct ContentView: View {
             }
             .id("section2")
 
-            // FEATURED PRODUCTS SECTION
+            // MARK: - PRODUCTS
             SectionView(title: "Featured Products", backgroundColor: .teal, textColor: .black) {
                 TabView {
-                    productView(title: "Festive Fireside Blend, C$5.00", imageName: "festive-fireside", message: "I would like to buy Festive Fireside Blend")
-                    productView(title: "Lemon & Ginger, C$5.00", imageName: "lemon-ginger", message: "I would like to buy Lemon & Ginger")
-                    productView(title: "Warm Chai, C$5.00", imageName: "warm-chai", message: "I would like to buy Warm Chai")
-                    productView(title: "Pumpkin Spice, C$5.00", imageName: "pumpkin-spice", message: "I would like to buy Pumpkin Spice")
+                    productView(title: "Festive Fireside Blend, C$5.00", imageName: "festive-fireside", description: "description", message: "I would like to buy Festive Fireside Blend")
+                    productView(title: "Lemon & Ginger, C$5.00", imageName: "lemon-ginger", description: "description", message: "I would like to buy Lemon & Ginger")
+                    productView(title: "Warm Chai, C$5.00", imageName: "warm-chai", description: "description", message: "I would like to buy Warm Chai")
+                    productView(title: "Pumpkin Spice, C$5.00", imageName: "pumpkin-spice", description: "description", message: "I would like to buy Pumpkin Spice")
                 }
                 .frame(height: 300)
                 .tabViewStyle(.page)
             }
             .id("section3")
 
-            // DELIVERY & PICKUP SECTION
+            // MARK: - DELIVERY
             SectionView(title: "Delivery & Pickup", backgroundColor: .white, textColor: .black) {
                 Text("FREE DELIVERY AND PICKUP TO OAKBANK RESIDENTS ONLY. Orders will be delivered within 3 days. Please contact us at truenortherntea@gmail.com to schedule pickup.")
                     .font(.custom("Georgia", size: 16))
@@ -122,7 +151,7 @@ struct ContentView: View {
             }
             .id("section4")
 
-            // ABOUT US SECTION
+            // MARK: - ABOUT
             SectionView(title: "About Us", backgroundColor: .gray, textColor: .black) {
                 Text("I’m Levi, a 15-year-old with a passion to make natural and healthy tea blends. At True Northern Tea, we use only the finest natural ingredients to create delicious yet nourishing and beneficial teas. Our mission is to make tea that tastes amazing but contains pure and simple natural ingredients while being convenient and at an affordable cost. Thank you for joining me on this journey!")
                     .font(.custom("Georgia", size: 16))
@@ -131,7 +160,7 @@ struct ContentView: View {
             }
             .id("section5")
 
-            // CONTACT US SECTION
+            // MARK: - CONTACT
             SectionView(title: "Contact Us", backgroundColor: .white, textColor: .black) {
                 VStack {
                     Link("Manitoba, Canada", destination: URL(string: "https://www.google.ca/maps/place/Manitoba/")!)
@@ -141,13 +170,17 @@ struct ContentView: View {
             }
             .id("section6")
 
-            // FOOTER
+            // MARK: - FOOTER
             footerView
         }
         .edgesIgnoringSafeArea(.top)
+        .overlay(
+            confirmationPopup, // Show confirmation popup when adding items
+            alignment: .top
+        )
     }
 
-    // Custom views
+    // MARK: - MENU
 
     func menuLink(_ text: String, sectionId: String) -> some View {
         Button(action: {
@@ -166,8 +199,107 @@ struct ContentView: View {
         .background(Color.gray.opacity(0.1))
         .cornerRadius(8)
     }
+    
+    // MARK: - CART
 
-    func productView(title: String, imageName: String, message: String) -> some View {
+    var cartPopup: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Cart")
+                .font(.custom("HelveticaNeue-Bold", size: 24))
+                .padding(.bottom, 10)
+
+            if cartItems.isEmpty {
+                Text("Your cart is empty")
+                    .font(.custom("HelveticaNeue", size: 16))
+                    .foregroundColor(.gray)
+            } else {
+                ForEach(cartItems) { item in
+                    HStack {
+                        Image(item.image)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(5)
+                        VStack(alignment: .leading) {
+                            Text(item.title)
+                                .font(.custom("HelveticaNeue", size: 16))
+                            Text(item.description)
+                                .font(.custom("HelveticaNeue", size: 14))
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.vertical, 5)
+                }
+
+                Button(action: {
+                    let totalCost = cartItems.count * 5 // Assuming each item is $5. Update as needed.
+                    let itemNames = cartItems.map { $0.title }.joined(separator: ", ")
+                    let smsBody = "I would like to purchase \(itemNames) for C$\(totalCost)."
+                    let sms = "sms:2044038767&body=\(smsBody)"
+                    
+                    if let url = URL(string: sms) {
+                        UIApplication.shared.open(url)
+                    }
+                    cartItems.removeAll() // Clear the cart after ordering
+                }) {
+                    Text("Order Now")
+                        .font(.custom("HelveticaNeue-Bold", size: 18))
+                        .padding(.horizontal, 30)
+                        .padding(.vertical, 10)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                }
+            }
+
+            Button(action: {
+                showCart.toggle() // Close the cart
+            }) {
+                Text("Close")
+                    .font(.custom("HelveticaNeue", size: 16))
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 10)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(15)
+        .shadow(radius: 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .transition(.move(edge: .bottom))
+    }
+    
+    // MARK: - CONFIRMATION
+
+    var confirmationPopup: some View {
+        VStack {
+            if showAddConfirmation {
+                Text("Item added to cart!")
+                    .font(.custom("HelveticaNeue-Bold", size: 16))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(8)
+                    .shadow(radius: 5)
+                    .transition(.opacity)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showAddConfirmation = false
+                            }
+                        }
+                    }
+            }
+        }
+        .padding(.top, 50) // Position it near the top
+    }
+
+    // MARK: - VIEWS
+    func productView(title: String, imageName: String, description: String, message: String) -> some View {
         VStack(spacing: 10) {
             Text(title)
                 .font(.custom("HelveticaNeue", size: 16))
@@ -178,12 +310,13 @@ struct ContentView: View {
                 .shadow(radius: 5)
 
             Button(action: {
-                let sms = "sms:2044038767&body=\(message)"
-                if let url = URL(string: sms) {
-                    UIApplication.shared.open(url)
+                let newItem = CartItem(title: title, message: message, image: imageName, description: description)
+                cartItems.append(newItem)
+                withAnimation {
+                    showAddConfirmation = true // Show confirmation popup
                 }
             }) {
-                Text("Buy Now")
+                Text("Add to Cart")
                     .font(.custom("HelveticaNeue-Bold", size: 16))
                     .padding()
                     .background(Color.white)
@@ -214,7 +347,7 @@ struct ContentView: View {
     }
 }
 
-// Custom SectionView to reduce repetitive code
+// MARK: - SECTION VIEW
 struct SectionView<Content: View>: View {
     var title: String
     var backgroundColor: Color
@@ -238,7 +371,7 @@ struct SectionView<Content: View>: View {
     }
 }
 
-// Preview for the SwiftUI ContentView
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
